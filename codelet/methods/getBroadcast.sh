1e074d3948ca89d0eaa3cc0f53bb1c39a43b93f7
wandgibaut <wandgibaut@github.com>
2020-03-04 23:44:44 -0300

    getOutput working

---
+++ codelet/methods/getBroadcast.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Returns the codelet broadcast
+# usage: ./getBroadcast.sh
+
+result= python3 readField.py broadcast
+
+# use the following commands to retrieve and store the result: 
+# result=$(./getBroadcast.sh)
+# echo $result
---
+++ codelet/methods/getInput.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Returns the codelet inputs
+# usage: ./getInputs.sh
+
+result= python3 readField.py inputs
+
+# use the following commands to retrieve and store the result: 
+# result=$(./getInputs.sh)
+# echo $result
