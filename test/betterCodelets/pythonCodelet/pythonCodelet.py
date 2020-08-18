be6ffeccda57747b3911dd45cb56797210d2aa77
wander <wander.gibaut@gmail.com>
2020-08-18 14:38:38 -0300

    ignore

--- test/betterCodelets/pythonCodelet/pythonCodelet.py
+++ test/betterCodelets/pythonCodelet/pythonCodelet.py
@@ -1,4 +1,4 @@
-#*****************************************************************************#
+# *****************************************************************************#
 # Copyright (c) 2020  Wandemberg Gibaut                                       #
 # All rights reserved. This program and the accompanying materials            #
 # are made available under the terms of the MIT License                       #
@@ -8,38 +8,37 @@
 # Contributors:                                                               #
 #      W. Gibaut                                                              #
 #                                                                             #
-#*****************************************************************************#
+# *****************************************************************************#
 import json
 import sys
 import socket
 import os
 import time
 import subprocess
-import dct # the dct utility module
+import dct  # the dct utility module
 import redis
 from pymongo import MongoClient
 
-#root_codelet_dir= os.getenv('root_codelet_dir')
 
-class pythonCodelet:
+# root_codelet_dir= os.getenv('root_codelet_dir')
+
+class PythonCodelet:
     def __init__(self, name=None, root_codelet_dir=os.getenv('root_codelet_dir')):
         self.root_codelet_dir = root_codelet_dir
         self.name = name
 
-    #TODO: getters and setters
+    # TODO: getters and setters
     def check_field(self, field):
         return subprocess.check_output(["python3", self.root_codelet_dir + "/methods/readField.py", field])
 
-
     def run(self):
-        while(bool(self.check_field('enable'))):
-            while(not bool(self.check_field('lock'))):
+        while (bool(self.check_field('enable'))):
+            while (not bool(self.check_field('lock'))):
                 activation = self.calculate_activation()
                 self.proc(activation)
                 time.sleep(float(self.check_field('timestep')))
 
         sys.exit()
-
 
     def calculate_activation(self):
         ########################################
@@ -49,20 +48,15 @@ class pythonCodelet:
         ## End of main code! ## 
         ########################################
 
-
-
     def proc(self, activation):
         ########################################
         ## Write you code below! ##
         print("default proc")
-        
+
         ## End of main code! ## 
         ########################################
-
 
 
 if __name__ == '__main__':
     codelet = pythonCodelet(name='defaultCodelet')
     codelet.run()
-        
-
