b6aaf3699340cd1d22a09f098363edd1ae0feb57
wander <wander.gibaut@gmail.com>
2020-08-24 22:53:36 -0300

    implemented a shell function to check node integrity

--- devel/mind_test/nodes/node2/nodeMaster.sh
+++ devel/mind_test/nodes/node2/nodeMaster.sh
@@ -12,28 +12,65 @@
 #                                                                             #
 #*****************************************************************************#
 
+check_integrity()
+{
+  for PID in "${PIDARRAY[@]}"
+  do
+    if [[ -d "/proc/${PID}" ]]
+    then
+      echo "process ${PID} exists"
+    else
+      regenerate_process "$PID"
+    fi
+  done
+}
+
+regenerate_process()
+{
+  for i in "${!PIDARRAY[@]}"; do
+    if [[ "${PIDARRAY[$i]}" = "$1" ]]; then
+      if [[ "${NAMEARRAY["$i"]}"  = "server" ]]; then
+        python3 "$ROOT_NODE_DIR"/server.py "$var"  &
+        PIDARRAY["$i"]=$!
+      else
+        python3 "$ROOT_NODE_DIR"/codelets/"${NAMEARRAY["$i"]}"/codelet.py  &
+        PIDARRAY["$i"]=$!
+        echo "regenerated ${NAMEARRAY[$i]}"
+      fi
+    fi
+  done
+}
+
+# array to index all pids
+PIDARRAY=()
+NAMEARRAY=()
 # initialize any number of servers, usually just one
 if [ $# -ne 0 ]
     then
         echo "initiating servers!"
         for var in "$@"
         do
-            python3 $ROOT_NODE_DIR/server.py "$var"  &
+            NAMEARRAY+=("server")
+            python3 "$ROOT_NODE_DIR"/server.py "$var"  &
+            PIDARRAY+=($!)
         done
 fi
 
 # run all codelet that should run
-mapfile -t <$ROOT_NODE_DIR/init_codelets.txt
+mapfile -t <"$ROOT_NODE_DIR"/init_codelets.txt
 
 for var in "${MAPFILE[@]}"
 do
-  python3 $ROOT_NODE_DIR/codelets/$var/codelet.py  &
+  NAMEARRAY+=("$var")
+  python3 "$ROOT_NODE_DIR"/codelets/"$var"/codelet.py  &
+  PIDARRAY+=($!)
 done
 
 # periodically check the health
 # TODO: implement caring routines
 while true
 do
-  sleep 1
+  check_integrity
+  sleep 10 #check every 10 secs
 done
 
