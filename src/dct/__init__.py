db2b80816ed50a9bdb4303460b5ca6b01701196d
Wandemberg Gibaut <wander.gibaut@gmail.com>
2022-10-05 21:14:07 -0300

    about to do a breaking change

--- src/dct/__init__.py
+++ src/dct/__init__.py
@@ -1,5 +1,5 @@
 # ****************************************************************************#
-# Copyright (c) 2020  Wandemberg Gibaut                                       #
+# Copyright (c) 2022  Wandemberg Gibaut                                       #
 # All rights reserved. This program and the accompanying materials            #
 # are made available under the terms of the MIT License                       #
 # which accompanies this distribution, and is available at                    #
@@ -9,143 +9,82 @@
 #      W. Gibaut                                                              #
 #                                                                             #
 # ****************************************************************************#
+
 import ujson as json
-import sys
-import os
-import time
 import socket
-import socketserver
 import redis
 from pymongo import MongoClient
 
 
-class PythonCodelet:
-    def __init__(self, name=None, root_codelet_dir=None):
-        if root_codelet_dir is None:
-            os.chdir(os.path.dirname(__file__))
-            root_codelet_dir = os.getcwd()
-        self.root_codelet_dir = root_codelet_dir
-        self.name = name
 
-    def read_field(self, field):
-        with open(self.root_codelet_dir + '/fields.json', 'r') as json_data:
-            jsonData = json.load(json_data)
-            value = jsonData[field]
-        return value
-
-    def change_field(self, field, value):
-        with open(self.root_codelet_dir + '/fields.json', 'r+') as json_data:
-            jsonData = json.load(json_data)
-            jsonData[field] = value
-            # print(jsonData[field])
-
-            json_data.seek(0)  # rewind
-            json.dump(jsonData, json_data)
-            json_data.truncate()
-
-    def add_entry(self, field, data):
-        with open(self.root_codelet_dir + '/fields.json', 'r+') as json_data:
-            jsonData = json.load(json_data)
-            vector = jsonData[field]
-            vector.append(json.loads(data))
-            jsonData[field] = vector
-
-            json_data.seek(0)  # rewind
-            json.dump(jsonData, json_data)
-            json_data.truncate()
-
-    def remove_entry(self, field, name):
-        with open(self.root_codelet_dir + '/fields.json', 'r+') as json_data:
-            jsonData = json.load(json_data)
-            vector = jsonData[field]
-
-            for i in vector:
-                for k, v in i.items():
-                    if v == name:
-                        vector.remove(i)
-                        return i
-
-            jsonData[field] = vector
-            # print(jsonData[field])
-
-            json_data.seek(0)  # rewind
-            json.dump(jsonData, json_data)
-            json_data.truncate()
-
-    def set_field_list(self, field, dataList):
-        jsonList = []
-        for dataString in dataList:
-            jsonList.append(json.loads(dataString))
-
-        with open(self.root_codelet_dir + '/fields.json', 'r+') as json_data:
-            jsonData = json.load(json_data)
-            jsonData[field] = jsonList
-            # print(jsonData[field])
-
-            json_data.seek(0)  # rewind
-            json.dump(jsonData, json_data)
-            json_data.truncate()
-
-    @staticmethod
-    def convert(self, string):
-        li = list(string.split(";"))
-        return li
-
-    def run(self):
-        while self.read_field('enable') == 'true':
-            while self.read_field('lock') == 'false':
-                activation = self.calculate_activation()
-                self.proc(activation)
-                time.sleep(float(self.read_field('timestep')))
-
-        sys.exit()
-
-    def calculate_activation(self):
-        ########################################
-        # Default Activation ##
-        print("default activation")
-        return 0
-
-    def proc(self, activation):
-        ########################################
-        # Default proc ##
-        print("default proc")
+def get_memory_object(memory_name, ip_port, conn_type):
+    if conn_type == 'mongo':
+        return get_mongo_memory(ip_port, memory_name)
+    elif conn_type == 'redis':
+        return get_redis_memory(ip_port, convert("/", memory_name)[1])
+    elif conn_type == 'tcp':
+        return get_tcp_memory(convert(":", ip_port)[0], convert(":", ip_port)[1], memory_name)
+    elif conn_type == 'local':
+        return get_local_memory(ip_port, memory_name)
+    return None
 
 
-def get_memory_objects(root_codelet_dir, memory_name, inputOrOutput):
+def set_memory_object(memory_name, ip_port, conn_type, field, value):
+    if conn_type == 'mongo':
+        return set_mongo_memory(ip_port, memory_name, field, value)
+    elif conn_type == 'redis':
+        return set_redis_memory(ip_port, convert("/", memory_name)[1], field, value)
+    elif conn_type == 'tcp':
+        return set_tcp_memory(convert(":", ip_port)[0], convert(":", ip_port)[1], memory_name, field, value)
+    elif conn_type == 'local':
+        return set_local_memory(ip_port, memory_name, field, value)
+    return None
+
+
+def get_memory_objects_by_name(root_codelet_dir, memory_name, inputOrOutput):
     with open(root_codelet_dir + '/fields.json', 'r+') as json_data:
         jsonData = json.load(json_data)
         vector = jsonData[inputOrOutput]
+        answer = []
         for entry in vector:
             if entry['name'] == memory_name:
-                if entry['type'] == 'mongo':
-                    return get_mongo_memory(entry['ip/port'], memory_name)
-                elif entry['type'] == 'redis':
-                    return get_redis_memory(entry['ip/port'], convert("/", memory_name)[1])
-                elif entry['type'] == 'tcp':
-                    return get_tcp_memory(convert(":", entry['ip/port'])[0], convert(":", entry['ip/port'])[1],
-                                          memory_name)
-                else:
-                    return get_local_memory(entry['ip/port'], entry['name'])
-
+                answer.append(get_memory_object(memory_name, entry['ip/port'], entry['type']))
+        if len(answer) != 0:
+            return answer
         return None
 
 
-def set_memory_objects(root_codelet_dir, memory_name, field, value, inputOrOutput):
+def set_memory_objects_by_name(root_codelet_dir, memory_name, field, value, inputOrOutput):
     with open(root_codelet_dir + '/fields.json', 'r+') as json_data:
         jsonData = json.load(json_data)
         vector = jsonData[inputOrOutput]
         for entry in vector:
             if entry['name'] == memory_name:
-                if entry['type'] == 'mongo':
-                    return set_mongo_memory(entry['ip/port'], memory_name, field, value)
-                elif entry['type'] == 'redis':
-                    return set_redis_memory(entry['ip/port'], convert("/", memory_name)[1], field, value)
-                elif entry['type'] == 'tcp':
-                    return set_tcp_memory(convert(":", entry['ip/port'])[0], convert(":", entry['ip/port'])[1],
-                                          memory_name, field, value)
-                else:  # local
-                    return set_local_memory(entry['ip/port'], memory_name, field, value)
+                set_memory_object(memory_name, entry['ip/port'], entry['type'], field, value)
+        return 0
+
+
+def get_memory_objects_by_group(root_codelet_dir, group, inputOrOutput):
+    with open(root_codelet_dir + '/fields.json', 'r+') as json_data:
+        jsonData = json.load(json_data)
+        vector = jsonData[inputOrOutput]
+        answer = []
+        for entry in vector:
+            if group in entry['group']:
+                answer.append(get_memory_object(entry['name'], entry['ip/port'], entry['type']))
+        if len(answer) != 0:
+            return answer
+        return None
+
+
+def set_memory_objects_by_group(root_codelet_dir, group, field, value, inputOrOutput):
+    with open(root_codelet_dir + '/fields.json', 'r+') as json_data:
+        jsonData = json.load(json_data)
+        vector = jsonData[inputOrOutput]
+        for entry in vector:
+            if group in entry['group']:
+                set_memory_object(entry['name'], entry['ip/port'], entry['type'], field, value)
+        return 0
 
 
 def get_all_memory_objects(root_codelet_dir, inputOrOutput):
@@ -154,32 +93,10 @@ def get_all_memory_objects(root_codelet_dir, inputOrOutput):
         vector = jsonData[inputOrOutput]
         answer = []
         for entry in vector:
-            answer.append(get_memory_objects(root_codelet_dir, entry['name'], inputOrOutput))
-        if answer:
+            answer.append(get_memory_object(entry['name'], entry['ip/port'], entry['type']))
+        if len(answer) != 0:
             return answer
         return None
-
-
-def get_memory_objects_group(root_codelet_dir, inputOrOutput, group):
-    with open(root_codelet_dir + '/fields.json', 'r+') as json_data:
-        jsonData = json.load(json_data)
-        vector = jsonData[inputOrOutput]
-        answer = []
-        for entry in vector:
-            if group in entry['group']:
-                answer.append(get_memory_objects(root_codelet_dir, entry['name'], inputOrOutput))
-        if answer:
-            return answer
-        return None
-
-
-def set_memory_objects_group(root_codelet_dir, field, value, inputOrOutput, group):
-    with open(root_codelet_dir + '/fields.json', 'r+') as json_data:
-        jsonData = json.load(json_data)
-        vector = jsonData[inputOrOutput]
-        for entry in vector:
-            if group in entry['group']:
-                set_memory_objects(root_codelet_dir, entry['name'], field, value, inputOrOutput)
 
 
 def get_redis_memory(host_port, memory_name):
@@ -250,10 +167,11 @@ def set_local_memory(path, memory_name, field, value):
         json_data.truncate()
 
 
+#TODO: solve this issue
 def add_memory_to_group(root_codelet_dir, memory_name, newGroup, inputOrOutput):
-    memory_group = get_memory_objects(root_codelet_dir, memory_name, inputOrOutput)['group']
+    memory_group = get_memory_objects_by_group(root_codelet_dir, memory_name, inputOrOutput)['group']
     memory_group.append(newGroup)
-    set_memory_objects(root_codelet_dir, memory_name, 'group', memory_group, inputOrOutput)
+    set_memory_objects_by_name(root_codelet_dir, memory_name, 'group', memory_group, inputOrOutput)
 
 
 def get_node_info(host, port):
